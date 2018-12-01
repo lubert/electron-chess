12e32a020abe8f010f3a19de8b09699e464bd9da
lubert <misterzhu@gmail.com>
2018-12-01 13:59:28 -0800

    Remove react-draggable

--- app/components/Piece.js
+++ app/components/Piece.js
@@ -1,18 +1,32 @@
 // @flow
 import React from 'react';
-import Draggable from 'react-draggable';
-import styles from './Piece.css';
+import './Piece.css';
 
 type Props = {
-  type: string,
-  x: number,
-  y: number
+  type: string
 };
 
-export default function Piece({ type, x, y }: Props) {
+const imgMap = {
+  K: require('../img/2d/wk.svg'),
+  Q: require('../img/2d/wq.svg'),
+  B: require('../img/2d/wb.svg'),
+  N: require('../img/2d/wn.svg'),
+  R: require('../img/2d/wr.svg'),
+  P: require('../img/2d/wp.svg'),
+  k: require('../img/2d/bk.svg'),
+  q: require('../img/2d/bq.svg'),
+  b: require('../img/2d/bb.svg'),
+  n: require('../img/2d/bn.svg'),
+  r: require('../img/2d/br.svg'),
+  p: require('../img/2d/bp.svg')
+};
+
+export default function Piece({ type }: Props) {
+  const img = imgMap[type];
+  if (!img) return null;
   return (
-    <Draggable>
-      <div className={styles[type]} style={{ top: y, left: x }} />
-    </Draggable>
+    <div>
+      <img src={img} alt={type} />
+    </div>
   );
 }
