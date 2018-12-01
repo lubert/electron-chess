e743577975bfdadc6549022c35b0acaa93816cbf
lubert <misterzhu@gmail.com>
2018-11-30 23:11:48 -0800

    Add GameWindow

---
+++ app/containers/GameWindow.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import styles from './GameWindow.css';
+
+export default class GameWindow extends Component {
+  render() {
+    return <div className={styles.text}>Hello</div>;
+  }
+}
