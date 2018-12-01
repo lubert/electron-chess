17883a3b6101b3bb687ef04369a5333b14753d4e
lubert <misterzhu@gmail.com>
2018-12-01 10:34:49 -0800

    Add translate style

--- app/components/Piece.js
+++ app/components/Piece.js
@@ -4,9 +4,16 @@ import React from 'react';
 import styles from './Piece.css';
 
 type Props = {
-  type: string
+  type: string,
+  x: number,
+  y: number
 };
 
-export default function Piece({ type }: Props) {
-  return <div className={styles[type]} />;
+export default function Piece({ type, x = 0, y = 0 }: Props) {
+  return (
+    <div
+      className={styles[type]}
+      style={{ transform: `translate(${x}px, ${y}px)` }}
+    />
+  );
 }
